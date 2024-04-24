14fabd3ef40b09c23cb8701e5e9be25461a478b6
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-25 00:06:08 +0900

    ログイン実装

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,81 @@
-import { Hono } from "hono";
+import { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
-import { z } from "zod";
+import { Hono } from "hono";
 import { bearerAuth } from "hono/bearer-auth";
+import { z } from "zod";
 import { users } from "./schema";
 
 type Bindings = {
-  DB: D1Database;
+	DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 // user作成(signup)のスキーマ
 const userSingupSchema = z.object({
-  name: z.string(),
-  email: z.string(),
-  password: z.string(),
+	name: z.string(),
+	email: z.string(),
+	password: z.string(),
 });
 
 // user作成(signup)
 app.post("/api/signup", async (c) => {
-  const formData = await c.req.formData();
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
+	const formData = await c.req.formData();
+	const name = formData.get("name") as string;
+	const email = formData.get("email") as string;
+	const password = formData.get("password") as string;
 
-  const { success } = userSingupSchema.safeParse({ name, email, password });
+	const { success } = userSingupSchema.safeParse({ name, email, password });
 
-  if (!success) {
-    return c.json({ error: "Invalid input data" }, 400);
-  }
+	if (!success) {
+		return c.json({ error: "Invalid input data" }, 400);
+	}
 
-  const db = drizzle(c.env.DB);
-  const user = await db
-    .insert(users)
-    .values({ name, email, password })
-    .execute();
+	const db = drizzle(c.env.DB);
+	const user = await db
+		.insert(users)
+		.values({ name, email, password })
+		.execute();
 
-  return c.json({ message: "User created successfully" });
+	return c.json({ message: "User created successfully" });
 });
 
 // Bearer用のトークンを設定
 const token = "honoiscool";
 
-// app.use("/api/*", bearerAuth({ token }));
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+// ログイン用のスキーマ
+const loginSchema = z.object({
+	email: z.string(),
+	password: z.string(),
 });
+
+// ログイン
+app.post("/api/login", async (c) => {
+	const formData = await c.req.formData();
+	const email = formData.get("email") as string;
+	const password = formData.get("password") as string;
+
+	const { success } = loginSchema.safeParse({ email, password });
+
+	if (!success) {
+		return c.json({ error: "Invalid input data" }, 400);
+	}
+
+	const db = drizzle(c.env.DB);
+	// emailとpasswordが一致するユーザーを取得
+	const user = await db
+		.select()
+		.from(users)
+		.where(and(eq(users.id, 42), eq(users.name, "Dan")));
+
+	if (!user) {
+		return c.json({ error: "User not found" }, 404);
+	}
+
+	return c.json({ message: "User logged in successfully", token: token });
+});
+
+// Bearer認証を設定
+app.use("/api/auth/*", bearerAuth({ token }));
 
 export default app;
