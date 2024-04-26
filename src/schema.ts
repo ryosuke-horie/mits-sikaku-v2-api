82ab06e7dfeec9b8d84b36e85a221867a6066870
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-26 19:12:51 +0900

    signupから名前を排除

--- src/schema.ts
+++ src/schema.ts
@@ -3,7 +3,6 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
 	id: integer("id").primaryKey(),
-	name: text("name"),
 	email: text("email"),
 	password: text("password"),
 	createdAt: integer("created_at").default(sql`CURRENT_TIMESTAMP`),
